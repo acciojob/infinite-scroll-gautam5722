496aee2d9e27e901f52967278725d8ce2641ec68
Gautam Kumar Sharma <81625160+gautam5722@users.noreply.github.com>
2025-01-28 23:56:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+
+
+var listItem = document.querySelector('#infi-list');
+
+var nextItem = 1;
+var loadMore = function(){
+    for(var i = 1; i <= 20; i++)
+    {
+        var item = document.createElement('li');
+        item.innerText = 'Item ' + i;
+        listItem.appendChild(item);
+    }
+}
+
+listItem.addEventListener('scroll', function(){
+    if(listItem.scrollTop + listItem.clientHeight >= listItem.scrollHeight){
+        loadMore();
+    }
+});
+
+
+loadMore();
+
 
